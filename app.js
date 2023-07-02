7b53c74ebe321e1fc178c91f4d5ec0ff2267beb9
LingDong12 <105384028+LingDong12@users.noreply.github.com>
2023-07-02 16:11:06 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
+
+            $(function () {
+                $('.example-popover').popover({
+                    container: 'body'
+                })
+            })
+            $(function(){
+            $('.popover-dismiss').popover({
+                trigger: 'focus'
+            })
+ });
+            $(function () {
+                $('#carousel').carousel({
+                    interval: 10000
+                });
+            });
+
+
+       
